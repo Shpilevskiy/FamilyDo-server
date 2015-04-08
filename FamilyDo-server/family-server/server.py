b8d2b23a5fda90242a29e2a89e8507d4ff1d0e96
MrLokans <trikster1911@gmail.com>
2015-04-08 22:53:08 +0300

    Basic Note and Notebook objects created.

--- FamilyDo-server/family-server/server.py
+++ FamilyDo-server/family-server/server.py
@@ -2,9 +2,51 @@ from flask import Flask
 app = Flask(__name__)
 
 
+class Notebook(object):
+
+    def __init__(self):
+        self.notes = []
+        self.last_id = 0
+
+    def add_note(self, note):
+        self.notes.append(note)
+        note.id = self.last_id
+        self.last_id += 1
+
+    def get_note_by_id(self, note_id):
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+        raise ValueError("No note with id {}!".format(note_id))
+
+
+class Note(object):
+
+    def __init__(self, text, author):
+        self.text = text
+        self.author = author
+
+    def render_to_html(self):
+        return """
+<b>Author: </b> {name} 
+<br>
+<b>Text: </b>{text}
+""".format(name=self.author, text=self.text)
+
+
+@app.route("/note/<int:note_id>")
+def show_note(note_id):
+    return notebook.get_note_by_id(note_id).render_to_html()
+
+
 @app.route("/")
 def hello():
-    return "<b>Adventure has <i>begun</i>!</b>"
+    pass
+    # return a1.render_to_html()
 
 if __name__ == "__main__":
+    notebook = Notebook()
+    notebook.add_note(Note("Some text", "MrLokans"))
+    notebook.add_note(Note("Another text", "Arararat"))
+
     app.run()
